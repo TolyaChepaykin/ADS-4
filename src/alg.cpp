68f0117f3f70e233487f53e119663889c6660dd1
TolyaChepaykin <160387810+TolyaChepaykin@users.noreply.github.com>
2024-03-19 20:57:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for(int i = 0; i < len; ++i) {
-    for(int j = i + 1; j < len; ++j) {
-      if(arr[i] = value - arr[j] && i != j) {
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] = value - arr[j] && i != j) {
         count += 1;
       }
     }
